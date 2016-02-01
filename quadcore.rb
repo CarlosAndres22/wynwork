2c60e93cbdf4ed48315357fdb3e3f9ea5cd5698e
Stevie <steviepinero@gmail.com>
2016-02-01 10:36:37 -0500

    works in good measure not locally though

--- quadcore.rb
+++ quadcore.rb
@@ -2,12 +2,19 @@ class Quadrilateral
   def initialize(side1, side2, side3, side4)
     @sides = [side1, side2, side3, side4]
   end
+    def perimeter
+      sum = 0
+
+      @sides.each do |num|
+        sum += num
+      end
+      sum
+    end
 end
 
 class Rectangle < Quadrilateral
-  def initialize(side1_2, side3_4)
-    @longSide = [side1_2]
-    @shortSide = [side3_4]
+  def initialize(side1, side2)
+    @side1_2 = [side1, side2, side1, side2]
   end
 end
 
@@ -31,13 +38,29 @@ end
 
 
 
-def test
-p  quad = Quadrilateral.new(1,1,1,1)
-p  rectangle = Rectangle.new(1,1)
-p  trapezoid = Trapezoid.new(1,1,1)
-p  square = Square.new(1)
-p  rhombus = Rhombus.new(1)
-end
+# def test
+# p  quad = Quadrilateral.new(1,1,1,1)
+# p  rectangle = Rectangle.new(1,1)
+# p  trapezoid = Trapezoid.new(1,1,1)
+# p  square = Square.new(1)
+# p  rhombus = Rhombus.new(1)
+# end
+#
+#
+# test
 
+def test
+  quad = Quadrilateral.new(1,2,3,4)
+  rectangle = Rectangle.new(1,2)
+  trapezoid = Trapezoid.new(1,1,2)
+  square = Square.new(1)
+  rhombus = Rhombus.new(1)
+
+  puts quad.perimeter == 10
+  puts rectangle.perimeter == 6
+  puts trapezoid.perimeter == 6
+  puts square.perimeter == 4
+  puts rhombus.perimeter == 4
+end
 
 test
