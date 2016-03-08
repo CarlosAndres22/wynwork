079edb05215319c245371121393280cc4bae34c8
Stevie <steviepinero@gmail.com>
2016-03-08 14:04:57 -0500

    rails search practic

---
+++ MusicDistribution/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+artist_list = [
+              ['The Doors'],
+              ['The Doors'],
+              ['Pink Floyd'],
+              ['Ozzy Osbourne'],
+              ['DJ Dado'],
+              ['The Killers'],
+              ['The Who']
+              ]
+
+  artist_list.each do |name|
+    Artist.find_or_create_by( name: name )
+  end
